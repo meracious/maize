608822b6c6d8049bb5319f1ca4d14abdfb7594ce
grvxs <gauravsinghsisodia5@gmail.com>
2021-07-02 11:42:43 +0530

    Change code format

--- src/init/stivale.c
+++ src/init/stivale.c
@@ -1,31 +1,26 @@
-#include "init/stivale2.h"
 #include <stddef.h>
+#include "init/stivale2.h"
 
 /* function name: stivale2_get_tag
  * arguments: stivale2_struct*, uint64_t id
  * returns: tag with given id
  */
-void *
-stivale2_get_tag (struct stivale2_struct *stivale2_struct, uint64_t id)
-{
-  // get the first tag from linked list
-  struct stivale2_tag *current_tag = (void *)stivale2_struct->tags;
-  // loop through all tags
-  for (;;)
-    {
-      // if curent tag is NULL that means it is the last tag
-      if (current_tag == NULL)
-        {
-          return NULL;
+void* stivale2_get_tag(struct stivale2_struct* stivale2_struct, uint64_t id) {
+    // get the first tag from linked list
+    struct stivale2_tag* current_tag = (void*)stivale2_struct->tags;
+    // loop through all tags
+    for (;;) {
+        // if curent tag is NULL that means it is the last tag
+        if (current_tag == NULL) {
+            return NULL;
         }
-      // if current_tag's id matches the requested id, then return this tag
-      if (current_tag->identifier == id)
-        {
-          return current_tag;
+        // if current_tag's id matches the requested id, then return this tag
+        if (current_tag->identifier == id) {
+            return current_tag;
         }
-      // get the next tag
-      current_tag = (void *)current_tag->next;
+        // get the next tag
+        current_tag = (void*)current_tag->next;
     }
-  // no such tag found, bruh!!
-  return NULL;
+    // no such tag found, bruh!!
+    return NULL;
 }
