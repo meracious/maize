8a35fb4214b07d91ba1157d01e1ed47729be7dd3
grvxs <gauravsinghsisodia5@gmail.com>
2021-07-06 21:03:50 +0530

    Add itoa function

---
+++ src/libk/string.c
@@ -0,0 +1,29 @@
+#include <libk/string.h>
+
+char* itoa(int value, char* str, int base) {
+    char* rc;
+    char* ptr;
+    char* low;
+    if (base < 2 || base > 36) {
+        *str = '\0';
+        return str;
+    }
+    rc = ptr = str;
+    if (value < 0 && base == 10) {
+        *ptr++ = '-';
+    }
+    low = ptr;
+    do {
+        *ptr++ =
+            "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstu"
+            "vwxyz"[35 + value % base];
+        value /= base;
+    } while (value);
+    *ptr-- = '\0';
+    while (low < ptr) {
+        char temp = *low;
+        *low++ = *ptr;
+        *ptr-- = temp;
+    }
+    return rc;
+}
