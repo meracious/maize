9a9edf91180a19860ef3ea684544075422adf62d
grvxs <gauravsinghsisodia5@gmail.com>
2021-07-09 14:20:20 +0530

    refactor: simplify init_psf

--- src/drivers/tty/psf_parser.c
+++ src/drivers/tty/psf_parser.c
@@ -8,10 +8,8 @@ psf1_font font;
 
 void init_psf()
 {
-    psf1_header *header = (psf1_header *)&_binary_scripts_zap_vga16_psf_start;
-    void *glyphs = (void *)(header + _binary_scripts_zap_vga16_psf_start);
-    font.header = header;
-    font.glyphs = glyphs;
+    font.header = (psf1_header *)&_binary_scripts_zap_vga16_psf_start;
+    font.glyphs = (void *)font.header + _binary_scripts_zap_vga16_psf_start;
 }
 
 psf1_font *get_font()
