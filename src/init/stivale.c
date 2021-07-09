836ebcbd11c1be0aba9c1296311df5653aad9d71
grvxs <gauravsinghsisodia5@gmail.com>
2021-07-09 22:47:19 +0530

    refactor: stivale2_get_tag

--- src/init/stivale.c
+++ src/init/stivale.c
@@ -10,21 +10,14 @@ void *stivale2_get_tag(struct stivale2_struct *stivale2_struct, uint64_t id)
     // get the first tag from linked list
     struct stivale2_tag *current_tag = (void *)stivale2_struct->tags;
     // loop through all tags
-    for (;;)
+    while (current_tag)
     {
-        // if curent tag is NULL that means it is the last tag
-        if (current_tag == NULL)
-        {
-            return NULL;
-        }
-        // if current_tag's id matches the requested id, then return this tag
+        // if current_tag's id matches the requested id, then return the tag
         if (current_tag->identifier == id)
-        {
             return current_tag;
-        }
-        // get the next tag
+        // move to the next tag
         current_tag = (void *)current_tag->next;
     }
-    // no such tag found, bruh!!
+    // no such tag found
     return NULL;
 }
