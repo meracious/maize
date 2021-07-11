b99920cec41fc353afa0e2f61196cf084293712c
grvxs <gauravsinghsisodia5@gmail.com>
2021-07-11 13:33:59 +0530

    perf: remove duplicate init_framebuffer call

--- src/init/main.c
+++ src/init/main.c
@@ -1,12 +1,10 @@
 // This file is a part of Maize Kernel
 // Wrote by Ayush Yadav(clawbhaiya)
-#include <drivers/framebuffer.h>
 #include <drivers/tty/tty.h>
 #include <libk/utils.h>
 
 void kmain(struct stivale2_struct *bootinfo)
 {
-    init_framebuffer(bootinfo);
     init_tty(bootinfo);
     init_colors(0x282828, 0xcc241d, 0x98971a, 0xd79921, 0x458588, 0xb16286,
         0x689d6a, 0xa89984, 0x928374, 0xfb4934, 0xb8bb26, 0xfabd2f,
