69eaf5ed9bcffb4b376449c402251a17d5d14600
grvxs <gauravsinghsisodia5@gmail.com>
2021-07-14 14:55:00 +0530

    docs: psf_parser.c

--- src/drivers/tty/psf_parser.c
+++ src/drivers/tty/psf_parser.c
@@ -6,12 +6,19 @@ extern uint32_t _binary_scripts_zap_vga16_psf_end;
 
 psf1_font font;
 
+/* function name: init_psf
+ * arguments: nothing
+ * returns: nothing
+ */
 void init_psf()
 {
     font.header = (psf1_header *)&_binary_scripts_zap_vga16_psf_start;
     font.glyphs = (void *)font.header + _binary_scripts_zap_vga16_psf_start;
 }
-
+/* function name: get_font
+ * arguments: nothing
+ * returns: psf font
+ */
 psf1_font *get_font()
 {
     return &font;
