69f126799f00bd65c48a828983d8fcae7a1cdb97
grvxs <gauravsinghsisodia5@gmail.com>
2021-07-14 14:55:18 +0530

    docs: main.c

--- src/init/main.c
+++ src/init/main.c
@@ -1,6 +1,10 @@
 #include <drivers/tty/tty.h>
 #include <libk/utils.h>
 
+/* function name: kmain
+ * arguments: stivale2_struct*
+ * returns: none
+ */
 void kmain(struct stivale2_struct *bootinfo)
 {
     init_tty(bootinfo);
