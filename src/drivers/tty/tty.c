06e70b6f73482e8603ec4d8ce38e8ef40bbc6f5e
grvxs <gauravsinghsisodia5@gmail.com>
2021-07-15 15:17:43 +0530

    refactor: move init_colors to init_tty

--- src/drivers/tty/tty.c
+++ src/drivers/tty/tty.c
@@ -63,6 +63,9 @@ void init_colors(uint32_t black,
 void init_tty(struct stivale2_struct *bootinfo)
 {
     init_framebuffer(bootinfo);
+    init_colors(0x282828, 0xcc241d, 0x98971a, 0xd79921, 0x458588, 0xb16286,
+        0x689d6a, 0xa89984, 0x928374, 0xfb4934, 0xb8bb26, 0xfabd2f,
+        0x83a598, 0xd8369b, 0x8ec07c, 0xebdbb2);
     fb = get_fb_info();
     cx = 0;
     cy = 0;
